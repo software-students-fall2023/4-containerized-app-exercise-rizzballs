203dc2173f519a050c4cce41e96fdf8c46c7fc89
Ryan-Horng <rh3025@nyu.edu>
2023-11-26 22:24:37 -0500

    black and linted

--- web-app/app.py
+++ web-app/app.py
@@ -1,21 +1,35 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response, session
+"""Module designed to supplement front end webpage"""
 import os
+import datetime
+import sys
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    make_response,
+    session,
+)
 from pymongo import MongoClient
 import pymongo
-import datetime
 from bson.objectid import ObjectId
-import sys
 
-app = Flask('project4')
+app = Flask("project4")
 
-@app.route('/')
-def RootPage():
-    return render_template('root.html')
 
-@app.route('/anaylzeData', methods=['POST'])
-def anaylzeData():
+@app.route("/")
+def root_page():
+    """Root page route"""
+    return render_template("root.html")
+
+
+@app.route("/analyze_data", methods=["POST"])
+def anaylze_data():
+    """Analyze data by sending it to the ml client"""
     return
 
+
 if __name__ == "__main__":
-    PORT = os.getenv('PORT', 5000) 
-    app.run(debug=True,port=PORT)
+    PORT = os.getenv("PORT", 5000)
+    app.run(debug=True, port=PORT)
