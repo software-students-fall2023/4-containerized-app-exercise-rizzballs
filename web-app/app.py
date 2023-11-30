b274290a2e9d0731f3bb1c768d84431d63da832d
Kei Oshima <ko2091@nyu.edu>
2023-11-29 22:13:14 -0500

    fixing linting issues

--- web-app/app.py
+++ web-app/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request, jsonify
+
+"""
+app.py file that helps serve as the front end of our application
+"""
+
 import os
 import subprocess
+from flask import Flask, render_template, request, jsonify
+
 
 app = Flask("project4")
 
@@ -9,12 +15,18 @@ os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 
 @app.route("/")
-def RootPage():
+def root_page():
+    """
+    template to render root page
+    """
     return render_template("root.html")
 
 
 @app.route("/analyzeData", methods=["POST"])
-def analyzeData():
+def analyze_data():
+    """
+    Function to send genreated audio file to the machine learning client
+    """
     try:
         if "audio" not in request.files:
             return jsonify({"status": "error", "message": "No audio file provided"})
@@ -27,19 +39,29 @@ def analyzeData():
         result = subprocess.run(
             [
                 "C:\\Users\\Andrew - User\\AppData\\Local\\Microsoft\\WindowsApps\\python.exe",
-                "E:\\4-containerized-app-exercise-rizzballs\\machine-learning-client\\machine_learning_client.py",
-                audio_path,
+                "E:\\4-containerized-app-exercise-rizzballs\\machine-learning-client\\machine_learning_client.py", # pylint: disable=line-too-long
+                audio_path, # pylint: disable=line-too-long disable=trailing-whitespace
             ],
             capture_output=True,
             text=True,
-        )
+            check=True
+            )
+        if result.returncode == 0:
+            print("Process succeeded!")
+        else:
+            print("Process failed. Exit code:", result.returncode)
 
         return jsonify({"status": "success"})
 
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)})
+    except FileNotFoundError as e:
+        return jsonify({"status": "error", "message": f"File not found: {str(e)}"})
+
+    except subprocess.CalledProcessError as e:
+        return jsonify({"status": "error", "message": f"Subprocess failed: {str(e)}"})
+
+
 
 
 if __name__ == "__main__":
-    PORT = os.getenv("PORT", 5000)
+    PORT = os.getenv("PORT", "5000")
     app.run(debug=True, port=PORT)
