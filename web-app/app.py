b38b69dab795b48e4028513ba3bf7ef030d9ca92
Kei Oshima <ko2091@nyu.edu>
2023-11-29 21:12:38 -0500

    editing yaml file

--- web-app/app.py
+++ web-app/app.py
@@ -2,29 +2,37 @@ from flask import Flask, render_template, request, jsonify
 import os
 import subprocess
 
-app = Flask('project4')
+app = Flask("project4")
 
-app.config['UPLOAD_FOLDER'] = 'uploads'
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+app.config["UPLOAD_FOLDER"] = "uploads"
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
-@app.route('/')
+
+@app.route("/")
 def RootPage():
-    return render_template('root.html')
+    return render_template("root.html")
 
-@app.route('/analyzeData', methods=['POST'])
+
+@app.route("/analyzeData", methods=["POST"])
 def analyzeData():
     try:
-        if 'audio' not in request.files:
+        if "audio" not in request.files:
             return jsonify({"status": "error", "message": "No audio file provided"})
 
-        audio_file = request.files['audio']
-        audio_path = os.path.join(app.config['UPLOAD_FOLDER'], 'user_audio.wav')
+        audio_file = request.files["audio"]
+        audio_path = os.path.join(app.config["UPLOAD_FOLDER"], "user_audio.wav")
         audio_file.save(audio_path)
 
-
-        print("Audio file saved at:", audio_path)  
-        result = subprocess.run(["C:\\Users\\Andrew - User\\AppData\\Local\\Microsoft\\WindowsApps\\python.exe", "E:\\4-containerized-app-exercise-rizzballs\\machine-learning-client\\machine_learning_client.py", audio_path], capture_output=True, text=True)
-
+        print("Audio file saved at:", audio_path)
+        result = subprocess.run(
+            [
+                "C:\\Users\\Andrew - User\\AppData\\Local\\Microsoft\\WindowsApps\\python.exe",
+                "E:\\4-containerized-app-exercise-rizzballs\\machine-learning-client\\machine_learning_client.py",
+                audio_path,
+            ],
+            capture_output=True,
+            text=True,
+        )
 
         return jsonify({"status": "success"})
 
@@ -33,6 +41,5 @@ def analyzeData():
 
 
 if __name__ == "__main__":
-    PORT = os.getenv('PORT', 5000) 
+    PORT = os.getenv("PORT", 5000)
     app.run(debug=True, port=PORT)
-
