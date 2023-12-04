0d728525b8076f8517cd111239d1ac26f7cc31bf
andrew0022 <andrewhuang0202@gmail.com>
2023-12-03 21:29:09 -0500

    fix formatting

--- web-app/app_tests/test_app.py
+++ web-app/app_tests/test_app.py
@@ -2,24 +2,26 @@ import sys
 import os
 import tempfile
 import pytest
-from flask import jsonify 
+from flask import jsonify
 from flask import Flask
 from flask import render_template
 from app import app, collection
 
 client = app.test_client()
 
+
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
+
 
 def test_root_page(client):
     response = client.get("/test_render_template")
     print("Response status code:", response.status_code)
     print("Response data:", response.data)
-    assert b'Recording audio...' in response.data
+    assert b"Recording audio..." in response.data
 
 
 def test_analyze_data(client, monkeypatch):
@@ -41,6 +43,7 @@ def test_analyze_data(client, monkeypatch):
         response = client.post("/analyzeData", data={"audio": (temp_file, "test.wav")})
         assert response.status_code == 200
         assert b"mocked" in response.data
+
 
 def test_analyze_data_no_audio(client):
     response = client.post("/analyzeData")
@@ -67,6 +70,7 @@ def test_analyze_data_failed_request(client, monkeypatch):
         response = client.post("/analyzeData", data={"audio": (temp_file, "test.wav")})
         assert response.status_code == 500
         assert b"Failed to send and process audio" in response.data
+
 
 if __name__ == "__main__":
     pytest.main(["-v", "test_app.py", "--cov=web-app"])
